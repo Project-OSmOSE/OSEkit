0744b76ae702a615732a86e15774107ed374d909
= <benjamin.loire@ensta-bretagne.org>
2023-03-29 11:40:47 +0200

    fix typo

--- tests/test_resample.py
+++ tests/test_resample.py
@@ -20,7 +20,7 @@ def test_resample(input_dir: Path, output_dir: Path):
         for i in range(3):
             output_file = output_dir.joinpath(f"test{i}.wav")
             assert output_file.is_file()
-            assert sf.info(output_file).sample_rate == sr
+            assert sf.info(output_file).samplerate == sr
             assert sf.info(output_file).channels == 1
             assert sf.info(output_file).frames == 900
             assert sf.info(output_file).duration == 3.0
