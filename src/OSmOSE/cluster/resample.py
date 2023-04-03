2ed34cf51f1f31394ab0a8d1cb713b0fa23b26e9
= <benjamin.loire@ensta-bretagne.org>
2023-04-03 15:29:23 +0200

    changing argument order

--- src/OSmOSE/cluster/resample.py
+++ src/OSmOSE/cluster/resample.py
@@ -26,8 +26,7 @@ def resample(
     # tfm.set_output_format(rate=target_sr)
 
     for audio_file in audio_files_list:
-        print(" ".join(["sox", str(audio_file), "-r", str(target_sr), "-t", "wavpcm", str(Path(output_dir, audio_file.name))]))
-        subprocess.run(["sox", str(audio_file), "-r", str(target_sr), "-t", "wavpcm", str(Path(output_dir, audio_file.name))], shell=True)
+        subprocess.run(["sox", str(audio_file), str(Path(output_dir, audio_file.name)), "-r", str(target_sr), "-t", "wavpcm"], shell=True)
 
         print(f"{audio_file.name} resampled to {target_sr}!")
     #     tfm.build_file(
