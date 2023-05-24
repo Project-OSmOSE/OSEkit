4d76784ff928ebde097bbb03606d710b6821aa1d
= <benjamin.loire@ensta-bretagne.org>
2023-05-24 13:10:53 +0200

    test update 1

--- tests/test_compute_statistics.py
+++ tests/test_compute_statistics.py
@@ -5,20 +5,20 @@ from pathlib import Path
 from OSmOSE.cluster import compute_stats
 
 
-def test_output_file_written(input_dir: Path, output_dir: Path):
+def test_output_file_written(input_dataset: Path, output_dir: Path):
     output_file = output_dir.joinpath("output.csv")
     print(output_file)
-    compute_stats(input_dir=input_dir, output_file=output_file, hp_filter_min_freq=100)
+    compute_stats(input_dir=input_dataset, output_file=output_file, hp_filter_min_freq=100)
 
     assert output_file.exists()
     assert output_file.stat().st_size > 0
 
 
-def test_output_file_columns(input_dir, output_dir: Path):
+def test_output_file_columns(input_dataset, output_dir: Path):
     output_file = output_dir.joinpath("output.csv")
     # Call the function to write normalization parameters
     compute_stats(
-        input_dir=str(input_dir), output_file=str(output_file), hp_filter_min_freq=100
+        input_dir=str(input_dataset), output_file=str(output_file), hp_filter_min_freq=100
     )
     # Check that the output file has the correct columns
     with open(str(output_file), "r") as f:
@@ -27,18 +27,18 @@ def test_output_file_columns(input_dir, output_dir: Path):
         assert header == ["filename", "mean", "std"]
 
 
-def test_output_file_content(input_dir, output_dir):
+def test_output_file_content(input_dataset, output_dir):
     output_file = output_dir / "output.csv"
     # Create two dummy WAV files with different means and stds
-    wav_file1 = input_dir / "test1.wav"
+    wav_file1 = input_dataset / "test1.wav"
     data1 = np.random.randn(1000) + 1
     sf.write(wav_file1, data1, 44100)
-    wav_file2 = input_dir / "test2.wav"
+    wav_file2 = input_dataset / "test2.wav"
     data2 = np.random.randn(1000) * 2
     sf.write(wav_file2, data2, 44100)
     # Call the function to write normalization parameters
     compute_stats(
-        input_dir=str(input_dir), output_file=str(output_file), hp_filter_min_freq=0
+        input_dir=str(input_dataset), output_file=str(output_file), hp_filter_min_freq=0
     )
     # Check that the output file has the correct content
     with open(str(output_file), "r") as f:
@@ -46,7 +46,7 @@ def test_output_file_content(input_dir, output_dir):
         header = next(reader)
         assert header == ["filename", "mean", "std"]
         row1 = next(reader)
-        assert row1[0] == str(input_dir / "test.wav")
+        assert row1[0] == str(input_dataset / "test.wav")
         # assert float(row1[1]) == pytest.approx(0, abs=1e-2)
         # assert float(row1[2]) == pytest.approx(1, abs=1e-2)
         row2 = next(reader)
