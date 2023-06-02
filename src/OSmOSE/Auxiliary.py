36242b90c5f53054233b40150e893e6f88970886
gmanatole <99678818+gmanatole@users.noreply.github.com>
2023-06-02 13:46:27 +0200

    Update Auxiliary.py

--- src/OSmOSE/Auxiliary.py
+++ src/OSmOSE/Auxiliary.py
@@ -161,6 +161,7 @@ class Variables():
 			self.timestamps = self.df['time']
 			self.depth = self.df['depth']
 		else :
+			print('I was here')
 			self.from_scratch()
 		self.local = local
 
@@ -250,7 +251,6 @@ class Variables():
 		Takes dictionary with lat, lon, depth of hydrophone and creates empty dataframe for class
 		Objective is to build time with timestamps
 		'''
-		print('I was here')
 		try : 
 			dataset = Dataset(Path(self.path, self.dataset), gps_coordinates=gps)
 			metadata = pd.read_csv(dataset._get_original_after_build().joinpath("metadata.csv"))
