d7023eda5d6c0dfae4ed16daded162e991ccda69
= <benjamin.loire@ensta-bretagne.org>
2023-06-08 16:44:50 +0200

    fix type issue (PosixPath => str)

--- src/OSmOSE/Dataset.py
+++ src/OSmOSE/Dataset.py
@@ -372,7 +372,7 @@ class Dataset:
             )
 
             with open(path_raw_audio.joinpath("files_not_loaded.csv"), "w") as fp:
-                fp.write("\n".join(list_filename_abnormal_duration))
+                fp.write("\n".join(str(list_filename_abnormal_duration)))
 
             return list_filename_abnormal_duration
 
