ae7e313cea03410512b612fba1cab31ee9ba1261
cazaudo <cazaudorian@outlook.fr>
2023-07-18 14:04:22 +0200

    correct time_vector definition

--- src/OSmOSE/Spectrogram.py
+++ src/OSmOSE/Spectrogram.py
@@ -1602,7 +1602,8 @@ class Spectrogram(Dataset):
         # make timestamps proper xitck_labels
         nber_xticks = min(10,log_spectro.shape[1])                 
         label_smoother = {'all':'D','Y':'M','M':'D','D':'T','H':'T'}
-        date = pd.date_range(start_time,end_time,periods=log_spectro.shape[1]).to_period(label_smoother[time_scale])  
+        time_vector = pd.date_range(start_time,end_time,periods=log_spectro.shape[1])
+        date = time_vector.to_period(label_smoother[time_scale])  
         int_sep = int(len(date) / nber_xticks)
         plt.xticks(np.arange(0, len(date), int_sep), date[::int_sep])
         ax.tick_params(axis='x', rotation=20)    
@@ -1612,9 +1613,8 @@ class Spectrogram(Dataset):
         plt.savefig(output_file, bbox_inches="tight", pad_inches=0)
         plt.close()
         
-        # Saving LTAS in npz format
-        
+        # Saving LTAS in npz format        
         output_file_npz=output_file.with_suffix('.npz')
-        np.savez(output_file_npz,LTAS=log_spectro,time=date,Freq=freq,allow_pickle=True)
+        np.savez(output_file_npz,LTAS=log_spectro,time=time_vector,Freq=freq,allow_pickle=True)
        
        
