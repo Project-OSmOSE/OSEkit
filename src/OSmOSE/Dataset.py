5c693323085c6324733fd376f127064cd338add9
cazaudo <cazaudorian@outlook.fr>
2023-08-24 14:23:27 +0200

    add depth info for fixed hydro (#105)
    * add depth info for fixed hydro
    
    * set depth and gps_coords as input args
    
    ---------
    
    Co-authored-by: gmanatole <99678818+gmanatole@users.noreply.github.com>

--- src/OSmOSE/Dataset.py
+++ src/OSmOSE/Dataset.py
@@ -34,8 +34,11 @@ class Dataset:
     def __init__(
         self,
         dataset_path: str,
+        gps_coordinates: Union[str, list, Tuple],
+        depth: Union[str, int],
         *,
         gps_coordinates: Union[str, list, Tuple] = None,
+        depth: Union[str, int] = None,
         owner_group: str = None,
         original_folder: str = None,
         local: bool = True,
@@ -69,9 +72,10 @@ class Dataset:
         self.owner_group = owner_group
         self.__gps_coordinates = []
         self.__local = local
-        if gps_coordinates is not None:
-            self.gps_coordinates = gps_coordinates
-
+        
+        self.gps_coordinates = gps_coordinates
+        self.depth = depth
+            
         self.__original_folder = original_folder
 
         if skip_perms:
@@ -113,7 +117,7 @@ class Dataset:
         Parameter
         ---------
         coordinates: `str` or `list` or `tuple`
-            If the coordinates are a string, it must be the name of a csv file located in `raw/auxiliary/`, containing two columns: 'lat' and 'long'
+            If the coordinates are a string, it must be the name of a csv file located in `data/auxiliary/instrument/`, containing two columns: 'lat' and 'lon'
             Else, they can be either a list or a tuple of two float, the first being the latitude and second the longitude; or a
             list or a tuple containing two lists or tuples respectively of floats. In this case, the coordinates are not treated as a point but
             as an area.
@@ -122,8 +126,6 @@ class Dataset:
         -------
         The GPS coordinates as a tuple.
         """
-        if not self.__gps_coordinates:
-            print("This dataset has no GPS coordinates.")
         return self.__gps_coordinates
 
     @gps_coordinates.setter
@@ -137,35 +139,62 @@ class Dataset:
             Tuple[Tuple[float, float], Tuple[float, float]],
         ],
     ):
-        # TODO: Allow any iterator?
+
         match new_coordinates:
             case str():
                 csvFileArray = pd.read_csv(
-                    self.path.joinpath(OSMOSE_PATH.auxiliary, new_coordinates)
+                    self.path.joinpath(OSMOSE_PATH.instrument, new_coordinates)
                 )
                 self.__gps_coordinates = [np.mean(csvFileArray["lat"]), np.mean(csvFileArray["lon"])
-                    #(np.min(csvFileArray["lat"]), np.max(csvFileArray["lat"])),
-                    #(np.min(csvFileArray["lon"]), np.max(csvFileArray["lon"])),
                 ]
             case tuple():
                 self.__gps_coordinates = new_coordinates
-            case list():
-                if all(isinstance(coord, list) for coord in new_coordinates):
-                    self.__gps_coordinates = (
-                        (new_coordinates[0][0], new_coordinates[0][1]),
-                        (new_coordinates[1][0], new_coordinates[1][1]),
-                    )
-                else:
-                    self.__gps_coordinates = (new_coordinates[0], new_coordinates[1])
-                # TODO : set a standard type for coordinates
-                # else:
-                #     raise ValueError(
-                #         f"The coordinates list must contain either only floats or only sublists of two elements."
-                #     )
             case _:
                 raise TypeError(
                     f"GPS coordinates must be either a list of coordinates or the name of csv containing the coordinates, but {type(new_coordinates)} found."
                 )
+
+
+    @property
+    def depth(
+        self,
+    ) -> int:
+        """The depth of the hydrophone, in meter.
+
+        Parameter
+        ---------
+        depth: `str` or `int`
+            If the depth is a string, it must be the name of a csv file located in `data/auxiliary/instrument/`, containing at least a column 'depth'
+
+        Returns
+        -------
+        The depth as an int.
+        """
+        return self.__depth
+    
+    @depth.setter
+    def depth(
+        self,
+        new_depth: Union[
+            str,
+            int,
+        ],
+    ):
+
+        match new_depth:
+            case str():
+                csvFileArray = pd.read_csv(
+                    self.path.joinpath(OSMOSE_PATH.instrument, new_depth)
+                )
+                self.__depth = int(np.mean(csvFileArray["depth"]))                
+            case int():
+                self.__depth = new_depth
+            case _:
+                raise TypeError(
+                    f"Variable depth must be either an int value for fixed hydrophone or a csv filename for moving hydrophone"
+                )
+                
+                
 
     @property
     def owner_group(self):
