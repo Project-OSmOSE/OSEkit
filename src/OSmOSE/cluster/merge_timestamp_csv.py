3feba5402d167db136ef083d00e6aeb377d5ce45
cazaudo <cazaudorian@outlook.fr>
2023-11-08 10:59:43 +0100

    remove batch timestamp csv files

--- src/OSmOSE/cluster/merge_timestamp_csv.py
+++ src/OSmOSE/cluster/merge_timestamp_csv.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import pandas as pd
 import itertools
 from argparse import ArgumentParser
+import os
 
 
 def merge_timestamp_csv(input_files: str ):
@@ -21,10 +22,12 @@ def merge_timestamp_csv(input_files: str ):
     list_conca_filename=[]
     for ll in list(input_dir_path.glob("timestamp_*")):
     
+        print(f"read and remove file {ll}")
         list_conca_timestamps.append(list(pd.read_csv(ll)['timestamp'].values))
         list_conca_filename.append(list(pd.read_csv(ll)['filename'].values))
-    
-        
+        os.remove(ll)
+            
+    print(f"save file {str(input_dir_path.joinpath('timestamp.csv'))}")
     df = pd.DataFrame({"filename": list(itertools.chain(*list_conca_filename)), "timestamp": list(itertools.chain(*list_conca_timestamps))})
     df.sort_values(by=["timestamp"], inplace=True)
     df.to_csv(
