342fbca9aeb852916c9951442acf0b6b52317ba4
Anatole GROS-MARTIAL <osmose@datarmor.com>
2024-05-21 14:45:45 +0000

    Added Auxiliary to the module's init

--- src/OSmOSE/__init__.py
+++ src/OSmOSE/__init__.py
@@ -3,4 +3,4 @@ from OSmOSE.timestamps import write_timestamp
 from OSmOSE.job import Job_builder
 from OSmOSE.Spectrogram import Spectrogram
 
-__all__ = ["Dataset", "write_timestamp", "Job_builder", "Spectrogram", "utils"]
+__all__ = ["Auxiliary", "Dataset", "write_timestamp", "Job_builder", "Spectrogram", "utils"]
