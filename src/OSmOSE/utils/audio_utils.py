acafaf59498c4998b3539b6861135d1e2e27d995
Mathieu Dupont <108517594+mathieudpnt@users.noreply.github.com>
2024-09-26 16:27:23 +0200

    fix is_audio and get_audio_file

--- src/OSmOSE/utils/audio_utils.py
+++ src/OSmOSE/utils/audio_utils.py
@@ -1,22 +1,16 @@
 from OSmOSE.config import SUPPORTED_AUDIO_FORMAT
 from pathlib import Path
-import glob
-import os
 
 
-def is_audio(filename: Path):
-    return any([filename.endswith(ext) for ext in SUPPORTED_AUDIO_FORMAT])
+def is_audio(filename: Path) -> bool:
+
+    return filename.suffix in SUPPORTED_AUDIO_FORMAT
 
 
-def get_audio_file(file_path: str | Path) -> list[Path]:
-
-    assert any(
-        [isinstance(file_path, str), isinstance(file_path, Path)]
-    ), "A Path or string must be provided"
+def get_audio_file(file_path: Path) -> list[Path]:
 
     audio_path = []
     for ext in SUPPORTED_AUDIO_FORMAT:
-        audio_path_ext = glob.glob(os.path.join(file_path, f"*{ext}"))
-        [audio_path.append(Path(f)) for f in audio_path_ext]
+        audio_path.extend(list(file_path.glob(f"*{ext}")))
 
     return sorted(audio_path)
