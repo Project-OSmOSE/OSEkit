9d9d7bc3dfe8316ac960fea6cfe265a6096585dd
Gautzilla <gauthier.berthomieu@gmail.com>
2024-10-17 17:49:08 +0200

    add function to check permissions before chmod

--- src/OSmOSE/utils/core_utils.py
+++ src/OSmOSE/utils/core_utils.py
@@ -745,3 +745,12 @@ def add_entry_for_APLOSE(path: str, file: str, info: pd.DataFrame):
             by=["path", "dataset"], ascending=True
         )
         meta.to_csv(dataset_csv, index=False)
+
+def chmod_if_needed(path: Path, mode: int):
+    if all(os.access(path, p) for p in (os.R_OK, os.W_OK)):
+        return
+
+    try:
+        os.chmod(path, mode)
+    except PermissionError:
+        raise PermissionError(f"You do not have the permission to write to {path}, nor to change its permissions.")
