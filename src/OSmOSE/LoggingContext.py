66795c9a61e24203b811f930f98dab83439c6ccf
Gautzilla <gauthier.berthomieu@gmail.com>
2024-10-25 11:52:59 +0200

    add logging context class

---
+++ src/OSmOSE/LoggingContext.py
@@ -0,0 +1,35 @@
+from contextlib import contextmanager
+import logging
+
+class LoggingContext:
+    def __init__(self):
+        self.logger = logging.root
+
+    @contextmanager
+    def set_logger(self, logger: logging.Logger):
+        """
+        This context manager can be used for calling utils functions with a specific logger.
+
+        Parameters
+        ----------
+        logger: logging.Logger
+            The logger to use in the function called within this context.
+            The function called should import the LoggingContext instance used for creating the context.
+
+        Examples
+        --------
+        global_logging_context = LoggingContext()
+        logger_to_use = logging.getLogger(__name__)
+
+        def log_something():
+            global_logging_context.logger.info("Info log")
+
+        with global_logging_context.set_logger(logger_to_use):
+            log_something() # This log will be sent to the logger_to_use logger
+        """
+        previous_logger = self.logger
+        try:
+            self.logger = logger
+            yield
+        finally:
+            self.logger = previous_logger
