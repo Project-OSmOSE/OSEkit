2792930635d0ed62fc7cc81155e53477cfbee6d0
Gautzilla <gauthier.berthomieu@gmail.com>
2024-12-03 14:43:45 +0100

    add AudioFile class

--- src/OSmOSE/audio_file.py
+++ src/OSmOSE/audio_file.py
@@ -1,4 +1,4 @@
-"""FileBase: Base class for the File objects (e.g. AudioFile), which associated timestamps with file-written data."""
+"""."""
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
@@ -7,18 +7,17 @@ if TYPE_CHECKING:
     from os import PathLike
 
     import numpy as np
-    from pandas import Timestamp
+import soundfile as sf
+from pandas import Timedelta, Timestamp
 
-from pathlib import Path
-
-from OSmOSE.utils.timestamp_utils import strptime_from_text
+from OSmOSE.file_base import FileBase
 
 
-class FileBase:
-    """Base class for the File objects (e.g. AudioFile), which associated timestamps with file-written data."""
+class AudioFile(FileBase):
+    """Audio file associated with timestamps."""
 
-    def __init__(self, path: PathLike | str, begin: Timestamp | None = None, end: Timestamp | None = None,  strptime_format: str | None = None) -> None:
-        """Initialize a File object with a path and timestamps.
+    def __init__(self, path: PathLike | str, begin: Timestamp | None = None, strptime_format: str | None = None) -> None:
+        """Initialize an AudioFile object with a path and a begin timestamp.
 
         The begin timestamp can either be provided as a parameter or parsed from the filename according to the provided strptime_format.
 
@@ -30,24 +29,18 @@ class FileBase:
             Timestamp corresponding to the first data point in the file.
             If it is not provided, strptime_format is mandatory.
             If both begin and strptime_format are provided, begin will overrule the timestamp embedded in the filename.
-        end: pandas.Timestamp | None
-            (Optional) Timestamp after the last data point in the file.
         strptime_format: str | None
             The strptime format used in the text.
             It should use valid strftime codes (https://strftime.org/).
             Example: '%y%m%d_%H:%M:%S'.
 
         """
-        self.path = Path(path)
-
-        if begin is None and strptime_format is None:
-            raise ValueError("Either begin or strptime_format must be specified")
-
-        self.begin = begin if begin is not None else strptime_from_text(text = self.path.name, datetime_template=strptime_format)
-        self.end = end if end is not None else self.begin
+        super().__init__(path, begin, strptime_format)
+        self.metadata = sf.info(path)
+        self.end = self.begin + Timedelta(seconds = self.metadata.duration)
 
     def read(self, start: Timestamp, stop: Timestamp) -> np.ndarray:
-        """Return the data that is between start and stop from the file.
+        """Return the audio data between start and stop from the file.
 
         Parameters
         ----------
@@ -58,6 +51,11 @@ class FileBase:
 
         Returns
         -------
-        The data between start and stop.
+        numpy.ndarray:
+            The audio data between start and stop.
 
         """
+        sample_rate = self.metadata.samplerate
+        start_sample = round((start-self.begin).total_seconds() * sample_rate)
+        stop_sample = round((stop-self.begin).total_seconds() * sample_rate)
+        return sf.read(self.path, start=start_sample, stop=stop_sample)[0]
