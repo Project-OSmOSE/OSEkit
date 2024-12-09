4816de0ad34e1a6ec3825bacb96c1e57cbaf4cd6
Gautzilla <gauthier.berthomieu@gmail.com>
2024-12-09 15:09:17 +0100

    add audio_data write method

--- src/OSmOSE/data/audio_data.py
+++ src/OSmOSE/data/audio_data.py
@@ -4,10 +4,12 @@ from __future__ import annotations
 
 import numpy as np
 
+from OSmOSE.config import TIMESTAMP_FORMAT_EXPORTED_FILES
 from OSmOSE.data.audio_item import AudioItem
 from OSmOSE.data.data_base import DataBase
 from OSmOSE.utils.audio_utils import resample
-
+import soundfile as sf
+from pathlib import Path
 
 class AudioData(DataBase):
     """AudioData encapsulating to a collection of AudioItem objects.
@@ -34,13 +36,17 @@ class AudioData(DataBase):
     @property
     def nb_channels(self) -> int:
         return max(
-            [1] + [item.nb_channels for item in self.items if type(item) is AudioItem]
+            [1] + [item.nb_channels for item in self.items if type(item) is AudioItem],
         )
 
     @property
     def shape(self):
         data_length = int(self.sample_rate * self.total_seconds)
         return data_length if self.nb_channels <= 1 else (data_length, self.nb_channels)
+
+    def __str__(self) -> str:
+        """Overwrite __str__."""
+        return self.begin.strftime(TIMESTAMP_FORMAT_EXPORTED_FILES)
 
     def _check_sample_rates(self, sample_rate: int | None = None) -> None:
         if sample_rate is not None or any(
@@ -60,6 +66,17 @@ class AudioData(DataBase):
             data[idx : idx + len(item_data)] = item_data
             idx += len(item_data)
         return data
+
+    def write(self, folder: Path) -> None:
+        """Write the audio data to file.
+
+        Parameters
+        ----------
+        folder: pathlib.Path
+            Folder in which to write the audio file.
+
+        """
+        sf.write(folder / f"{self}.wav" , self.get_value(), self.sample_rate)
 
     def _get_item_value(self, item: AudioItem) -> np.ndarray:
         item_data = item.get_value()
