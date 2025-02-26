80b6b0f8dbe45cc8ed4d831d42df12a530c43461
Gautzilla <gauthier.berthomieu@gmail.com>
2025-02-26 10:48:13 +0100

    add tests for AudioDataset.from_files

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -681,6 +681,107 @@ def test_audio_dataset_from_folder(
 
 
 @pytest.mark.parametrize(
+    ("audio_files", "begin", "end", "duration", "expected_audio_data"),
+    [
+        pytest.param(
+            {
+                "duration": 1,
+                "sample_rate": 48_000,
+                "nb_files": 1,
+                "date_begin": pd.Timestamp("2024-01-01 12:00:00"),
+                "series_type": "increase",
+            },
+            None,
+            None,
+            None,
+            generate_sample_audio(1, 48_000),
+            id="one_entire_file",
+        ),
+        pytest.param(
+            {
+                "duration": 1,
+                "sample_rate": 48_000,
+                "nb_files": 3,
+                "date_begin": pd.Timestamp("2024-01-01 12:00:00"),
+                "series_type": "increase",
+            },
+            None,
+            None,
+            pd.Timedelta(seconds=1),
+            generate_sample_audio(
+                nb_files=3,
+                nb_samples=48_000,
+                series_type="increase",
+            ),
+            id="multiple_consecutive_files",
+        ),
+        pytest.param(
+            {
+                "duration": 1,
+                "sample_rate": 48_000,
+                "nb_files": 2,
+                "inter_file_duration": 1,
+                "date_begin": pd.Timestamp("2024-01-01 12:00:00"),
+                "series_type": "increase",
+            },
+            None,
+            None,
+            pd.Timedelta(seconds=1),
+            [
+                generate_sample_audio(nb_files=1, nb_samples=96_000)[0][0:48_000],
+                generate_sample_audio(
+                    nb_files=1,
+                    nb_samples=48_000,
+                    min_value=0.0,
+                    max_value=0.0,
+                )[0],
+                generate_sample_audio(nb_files=1, nb_samples=96_000)[0][48_000:],
+            ],
+            id="two_separated_files",
+        ),
+        pytest.param(
+            {
+                "duration": 1,
+                "sample_rate": 48_000,
+                "nb_files": 3,
+                "inter_file_duration": -0.5,
+                "date_begin": pd.Timestamp("2024-01-01 12:00:00"),
+                "series_type": "repeat",
+            },
+            None,
+            None,
+            pd.Timedelta(seconds=1),
+            generate_sample_audio(nb_files=2, nb_samples=48_000),
+            id="overlapping_files",
+        ),
+    ],
+    indirect=["audio_files"],
+)
+def test_audio_dataset_from_files(
+    tmp_path: Path,
+    audio_files: tuple[list[Path], pytest.fixtures.Subrequest],
+    begin: pd.Timestamp | None,
+    end: pd.Timestamp | None,
+    duration: pd.Timedelta | None,
+    expected_audio_data: list[np.ndarray],
+) -> None:
+    afs = [
+        AudioFile(f, strptime_format=TIMESTAMP_FORMAT_TEST_FILES)
+        for f in tmp_path.glob("*.wav")
+    ]
+    dataset = AudioDataset.from_files(
+        afs,
+        begin=begin,
+        end=end,
+        data_duration=duration,
+    )
+    assert all(
+        np.array_equal(data.get_value(), expected)
+        for (data, expected) in zip(dataset.data, expected_audio_data)
+    )
+
+
+@pytest.mark.parametrize(
     (
         "audio_files",
         "expected_audio_data",
