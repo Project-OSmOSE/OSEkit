c891efe9e6bbae48ac2c8eb9bea2982af406f5c4
Gautzilla <gauthier.berthomieu@gmail.com>
2025-03-03 11:04:56 +0100

    add tests for SpectroData dictionary serialization

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -5,11 +5,15 @@ from typing import TYPE_CHECKING
 import numpy as np
 import pytest
 from pandas import Timedelta, Timestamp
+from scipy.signal import ShortTimeFFT
+from scipy.signal.windows import hamming, hann
 
-from OSmOSE.config import TIMESTAMP_FORMAT_TEST_FILES
+from OSmOSE.config import TIMESTAMP_FORMAT_EXPORTED_FILES, TIMESTAMP_FORMAT_TEST_FILES
 from OSmOSE.core_api.audio_data import AudioData
 from OSmOSE.core_api.audio_dataset import AudioDataset
 from OSmOSE.core_api.audio_file import AudioFile
+from OSmOSE.core_api.spectro_data import SpectroData
+from OSmOSE.core_api.spectro_file import SpectroFile
 
 if TYPE_CHECKING:
     from pathlib import Path
@@ -191,4 +195,154 @@ def test_audio_dataset_serialization(
     assert all(
         np.array_equal(ad.get_value(), ad2.get_value())
         for ad, ad2 in zip(ads.data, ads2.data)
+    )
+
+
+@pytest.mark.parametrize(
+    ("audio_files", "begin", "end", "sft", "sample_rate"),
+    [
+        pytest.param(
+            {
+                "duration": 1,
+                "sample_rate": 48_000,
+                "nb_files": 1,
+                "date_begin": Timestamp("2024-01-01 12:00:00"),
+            },
+            None,
+            None,
+            ShortTimeFFT(win=hamming(1024), hop=1024, fs=48_000, mfft=1024),
+            48_000,
+            id="full_file_no_resample",
+        ),
+        pytest.param(
+            {
+                "duration": 1,
+                "sample_rate": 48_000,
+                "nb_files": 1,
+                "date_begin": Timestamp("2024-01-01 12:00:00"),
+            },
+            None,
+            None,
+            ShortTimeFFT(win=hamming(1024, sym=False), hop=1024, fs=24_000, mfft=1024),
+            24_000,
+            id="non_symetric_hamming_window",
+        ),
+        pytest.param(
+            {
+                "duration": 1,
+                "sample_rate": 48_000,
+                "nb_files": 1,
+                "date_begin": Timestamp("2024-01-01 12:00:00"),
+            },
+            None,
+            None,
+            ShortTimeFFT(win=hann(1024), hop=1024, fs=24_000, mfft=1024),
+            24_000,
+            id="hann_window",
+        ),
+        pytest.param(
+            {
+                "duration": 1,
+                "sample_rate": 48_000,
+                "nb_files": 1,
+                "date_begin": Timestamp("2024-01-01 12:00:00"),
+            },
+            None,
+            None,
+            ShortTimeFFT(win=hamming(1024), hop=512, fs=24_000, mfft=1024),
+            24_000,
+            id="full_file_downsample_and_overlap",
+        ),
+        pytest.param(
+            {
+                "duration": 1,
+                "sample_rate": 48_000,
+                "nb_files": 1,
+                "date_begin": Timestamp("2024-01-01 12:00:00"),
+            },
+            None,
+            None,
+            ShortTimeFFT(win=hamming(1024), hop=1024, fs=96_000, mfft=2048),
+            96_000,
+            id="full_file_upsample_and_mfft_padding",
+        ),
+        pytest.param(
+            {
+                "duration": 3,
+                "sample_rate": 48_000,
+                "nb_files": 1,
+                "date_begin": Timestamp("2024-01-01 12:00:00"),
+            },
+            Timestamp("2024-01-01 12:00:01"),
+            Timestamp("2024-01-01 12:00:02"),
+            ShortTimeFFT(win=hamming(1024), hop=512, fs=48_000, mfft=2048),
+            48_000,
+            id="file_part_and_overlap_and_padding",
+        ),
+        pytest.param(
+            {
+                "duration": 1.5,
+                "sample_rate": 48_000,
+                "nb_files": 2,
+                "date_begin": Timestamp("2024-01-01 12:00:00"),
+            },
+            Timestamp("2024-01-01 12:00:01"),
+            Timestamp("2024-01-01 12:00:02"),
+            ShortTimeFFT(win=hamming(1024), hop=1024, fs=24_000, mfft=1024),
+            24_000,
+            id="two_files_with_resample",
+        ),
+        pytest.param(
+            {
+                "duration": 2,
+                "sample_rate": 48_000,
+                "nb_files": 2,
+                "inter_file_duration": 1,
+                "date_begin": Timestamp("2024-01-01 12:00:00"),
+            },
+            Timestamp("2024-01-01 12:00:01"),
+            Timestamp("2024-01-01 12:00:04"),
+            ShortTimeFFT(win=hamming(1024), hop=1024, fs=24_000, mfft=1024),
+            48_000,
+            id="two_files_with_gap",
+        ),
+    ],
+    indirect=["audio_files"],
+)
+def test_spectro_data_serialization(
+    tmp_path: Path,
+    audio_files: tuple[list[Path], pytest.fixtures.Subrequest],
+    begin: Timestamp | None,
+    end: Timestamp | None,
+    sft: ShortTimeFFT,
+    sample_rate: float,
+) -> None:
+    af = [
+        AudioFile(f, strptime_format=TIMESTAMP_FORMAT_TEST_FILES)
+        for f in tmp_path.glob("*.wav")
+    ]
+
+    ad = AudioData.from_files(af, begin=begin, end=end, sample_rate=sample_rate)
+
+    # SpectroData linked to AudioData
+
+    sd = SpectroData.from_audio_data(ad, sft)
+
+    assert np.array_equal(
+        sd.get_value(), SpectroData.from_dict(sd.to_dict(embed_sft=True)).get_value()
+    )
+
+    # SpectroData linked to SpectroFiles
+
+    sd.write(tmp_path)
+
+    sfs = [
+        SpectroFile(file, strptime_format=TIMESTAMP_FORMAT_EXPORTED_FILES)
+        for file in tmp_path.glob("*.npz")
+    ]
+
+    sd2 = SpectroData.from_files(sfs)
+
+    assert np.array_equal(
+        sd.get_value(), SpectroData.from_dict(sd2.to_dict(embed_sft=True)).get_value()
     )
