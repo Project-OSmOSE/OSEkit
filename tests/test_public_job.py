bcde81e0b08dee66b21c7b3ae193d3e8412d0bf6
Gautzilla <gauthier.berthomieu@gmail.com>
2025-10-09 10:39:07 +0200

    add job module docstrings

--- tests/test_public_job.py
+++ tests/test_public_job.py
@@ -1,28 +1,38 @@
-from osekit.public_api.job import Job, JobStatus
-import pytest
 from pathlib import Path
+
+import pytest
+
+from osekit.public_api.job import Job, JobStatus
 
 
 @pytest.mark.parametrize(
     ("initial_status", "expected_status"),
     [
         pytest.param(
-            JobStatus.UNPREPARED, JobStatus.PREPARED, id="unprepared_becomes_prepared"
+            JobStatus.UNPREPARED,
+            JobStatus.PREPARED,
+            id="unprepared_becomes_prepared",
         ),
         pytest.param(
-            JobStatus.PREPARED, JobStatus.QUEUED, id="prepared_becomes_queued"
+            JobStatus.PREPARED,
+            JobStatus.QUEUED,
+            id="prepared_becomes_queued",
         ),
         pytest.param(JobStatus.QUEUED, JobStatus.RUNNING, id="queued_becomes_running"),
         pytest.param(
-            JobStatus.RUNNING, JobStatus.COMPLETED, id="running_becomes_completed"
+            JobStatus.RUNNING,
+            JobStatus.COMPLETED,
+            id="running_becomes_completed",
         ),
         pytest.param(
-            JobStatus.COMPLETED, JobStatus.COMPLETED, id="completed_remains_completed"
+            JobStatus.COMPLETED,
+            JobStatus.COMPLETED,
+            id="completed_remains_completed",
         ),
     ],
 )
 def test_job_progress(initial_status: JobStatus, expected_status: JobStatus) -> None:
-    job = Job(script_path=Path(r""))
+    job = Job(script_path=Path())
     assert job.status == JobStatus.UNPREPARED
     job._status = initial_status
     job.progress()
