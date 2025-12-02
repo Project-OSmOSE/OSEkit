15b67f605f15c3973e66c9b04fee1a4827765f7b
Gautzilla <gauthier.berthomieu@gmail.com>
2025-12-02 17:16:24 +0100

    set file paths as relative to the JSON paths

--- src/osekit/core_api/json_serializer.py
+++ src/osekit/core_api/json_serializer.py
@@ -1,7 +1,42 @@
 """Functions used to serialize the API objects to json files."""
 
 import json
+import os
 from pathlib import Path
+from typing import Literal
+
+
+def set_path_reference(
+    serialized_dict: dict,
+    root_path: Path,
+    reference: Literal["absolute", "relative"],
+) -> None:
+    """Recursively set all paths either relative to root_path or absolute from root_path.
+
+    Parameters
+    ----------
+    serialized_dict: dict
+        Dictionary in which the path values must be modified.
+    root_path: Path
+        Root path:
+         - From which the relative files are expressed.
+         - From which the absolute files should be expressed as relative
+    reference: Literal["absolute","relative"]
+        How to express the file names:
+        "absolute": Files names are converted from relative to root_path to absolute.
+        "relative": Files names are converted from absolute to relative to root_path.
+
+    """
+    for key, value in serialized_dict.items():
+        if type(value) is dict:
+            set_path_reference(value, root_path, reference)
+            continue
+        if key == "path":
+            serialized_dict[key] = (
+                (root_path / value).resolve()
+                if reference == "absolute"
+                else os.path.relpath(value, root_path)
+            )
 
 
 def serialize_json(path: Path, serialized_dict: dict) -> None:
@@ -16,6 +51,11 @@ def serialize_json(path: Path, serialized_dict: dict) -> None:
 
     """
     path.parent.mkdir(parents=True, exist_ok=True)
+    set_path_reference(
+        serialized_dict=serialized_dict,
+        root_path=path.parent,
+        reference="relative",
+    )
     with path.open("w") as file:
         file.write(json.dumps(serialized_dict, sort_keys=True, indent=4))
 
@@ -35,4 +75,10 @@ def deserialize_json(path: Path) -> dict:
 
     """
     with path.open("r") as f:
-        return json.loads(f.read())
+        dictionary = json.loads(f.read())
+    set_path_reference(
+        serialized_dict=dictionary,
+        root_path=path.parent,
+        reference="absolute",
+    )
+    return dictionary
