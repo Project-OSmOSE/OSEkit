54b9cb850ff1ce353e6222528821e6f6934b4ca8
Gautzilla <gauthier.berthomieu@gmail.com>
2026-01-21 10:55:00 +0100

    add code blocks in instrument docstrings

--- src/osekit/core_api/instrument.py
+++ src/osekit/core_api/instrument.py
@@ -25,19 +25,19 @@ class Instrument:
         gain_db: float = 0.0,
         end_to_end_db: float | None = None,
     ) -> None:
-        """Initialize an Instrument object.
+        """Initialize an ``Instrument`` object.
 
         Parameters
         ----------
         sensitivity: float
             Sensitivity (in Volt per Pascal) of the sensor.
-            Defaulted to 1.
+            Defaulted to ``1.``.
         peak_voltage: float
-            Voltage that leads to a 0 dB FS signal.
-            Defaulted to 1 V.
+            Voltage that leads to a ``0 dB FS`` signal.
+            Defaulted to ``1 V``.
         gain_db: float
             Total gain (in dB) of the chain of amplifiers.
-            Defaulted to 0 dB.
+            Defaulted to ``0 dB``.
         end_to_end_db: float | None
             End-to-end calibration value, as given by e.g. SoundTrap datasheets.
             If provided, it will overwrite the former parameters.
@@ -61,7 +61,7 @@ class Instrument:
 
     @property
     def end_to_end_db(self) -> float:
-        """Total ratio between digital signal value and acoustic pressure level (re 1uPa)."""
+        """Total ratio between digital signal value and acoustic pressure level (re ``1uPa``)."""
         return 20 * np.log10(self.end_to_end / self.P_REF)
 
     @end_to_end_db.setter
@@ -74,7 +74,7 @@ class Instrument:
 
     @property
     def sensitivity(self) -> float:
-        """Sensitivity of the sensor, in V/Pa."""
+        """Sensitivity of the sensor, in ``V/Pa``."""
         return self._sensitivity
 
     @sensitivity.setter
@@ -83,7 +83,7 @@ class Instrument:
 
     @property
     def peak_voltage(self) -> float:
-        """Voltage that leads to a 0 dB FS signal."""
+        """Voltage that leads to a ``0 dB FS`` signal."""
         return self._peak_voltage
 
     @peak_voltage.setter
@@ -101,7 +101,7 @@ class Instrument:
 
     @property
     def gain_db(self) -> float:
-        """Total gain (in dB) of the chain of amplifiers."""
+        """Total gain (in ``dB``) of the chain of amplifiers."""
         return 20 * np.log10(self.gain)
 
     @gain_db.setter
@@ -109,7 +109,7 @@ class Instrument:
         self._g = 10 ** (value / 20)
 
     def n_to_p(self, digit_value: float) -> float:
-        """Convert raw digital data to acoustic pressure (in Pa).
+        """Convert raw digital data to acoustic pressure (in ``Pa``).
 
         Parameters
         ----------
@@ -119,7 +119,7 @@ class Instrument:
         Returns
         -------
         float:
-            Acoustic pressure (in Pa).
+            Acoustic pressure (in ``Pa``).
 
         """
         return digit_value * self.end_to_end
@@ -135,7 +135,7 @@ class Instrument:
 
     @classmethod
     def from_dict(cls, data: dict | None) -> Instrument | None:
-        """Deserialize an Instrument from a dictionary."""
+        """Deserialize an ``Instrument`` from a dictionary."""
         if data is None:
             return None
         return cls(**data)
