0be700caf9f205870d2cf595a0eb80620380ff85
Gautzilla <gauthier.berthomieu@gmail.com>
2026-07-10 11:26:27 +0200

    raise an error after parsing an empty audio file

--- src/osekit/core/audio_file.py
+++ src/osekit/core/audio_file.py
@@ -68,6 +68,16 @@ class AudioFile(BaseFile):
         self.sample_rate = sample_rate
         self.channels = channels
         self.end = self.begin + Timedelta(seconds=duration)
+        self._check_validity()
+
+    def _check_validity(self) -> None:
+        """Raise an error if the audio file is not valid."""
+        if not self.duration:
+            msg = (
+                f"Audio file {self.path} is empty.\n"
+                f"This error could be due to corrupted file metadata."
+            )
+            raise ValueError(msg)
 
     def read(self, start: Timestamp, stop: Timestamp) -> np.ndarray:
         """Return the audio data between start and stop from the file.
