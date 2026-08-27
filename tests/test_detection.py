792d4e86ab6b0c35865e737f57e20a53b0fb6653
Gautzilla <72027971+Gautzilla@users.noreply.github.com>
2026-08-27 10:14:44 +0200

    Detection to dict (#467)
    * add Detection.to_dict() tests
    
    * run Detection.to_dict() tests on both result files
    
    * remove redundant signal_quantity lookup
    
    * add dataclasses to_dict() methods
    
    * add Detection.to_dict() method

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import numpy as np
 import pytest
-from pandas import Timestamp
+from pandas import DataFrame, Timestamp
 
 from osekit.core.detection import (
     ConfidenceIndicator,
@@ -330,6 +330,23 @@ def test_detections_from_csv() -> None:
     assert minimal_detection.signal_quantity is None
     assert minimal_detection.signal_parameters is None
     assert len(minimal_detection.verifications) == 0
+
+
+def test_detections_to_dict(tmp_path: Path) -> None:
+    for result_file in ("aplose_result.csv", "minimal_detector_result.csv"):
+        detections = Detection.from_csv(
+            csv=Path(__file__).parent / "_static" / result_file,
+        )
+
+        for detection in detections:
+            assert Detection.from_dict(detection.to_dict()) == detection
+
+        # Output dict should be formatted as an APLOSE output dict
+        DataFrame([d.to_dict() for d in detections]).to_csv(tmp_path / "export.csv")
+        assert np.array_equal(
+            sorted(detections, key=str),
+            sorted(Detection.from_csv(tmp_path / "export.csv"), key=str),
+        )
 
 
 def test_detection_to_rectangle(sample_detection: Detection) -> None:
