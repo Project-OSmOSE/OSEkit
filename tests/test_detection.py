c1df4e49f2333d1274b0cbc9b2dffd10c01a8a36
Gautzilla <72027971+Gautzilla@users.noreply.github.com>
2026-09-10 12:13:39 +0200

    test that Label.get_text_size() removes the text from the axes after measuring it

--- tests/test_detection.py
+++ tests/test_detection.py
@@ -460,9 +460,9 @@ def test_label_get_size_depends_on_text_size(custom_axes: Axes) -> None:
     assert small_height < large_height
 
 
-def test_get_size_removes_artists_from_ax(custom_axes: Axes) -> None:
-    initial_artists = len(custom_axes.artists)
+def test_get_size_removes_text_from_ax(custom_axes: Axes) -> None:
+    initial_artists = len(custom_axes.texts)
 
     Label("cool").get_text_size(ax=custom_axes)
 
-    assert len(custom_axes.artists) == initial_artists
+    assert len(custom_axes.texts) == initial_artists
